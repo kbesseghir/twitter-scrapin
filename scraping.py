bb2cc64d6db349d4e704cbe44d04775187f235a7
kbesseghir <k.besseghir@esi-sba.dz>
2023-09-09 15:50:40 +0100

    final commit

--- scraping.py
+++ scraping.py
@@ -25,7 +25,7 @@ login_box = WebDriverWait(driver, 10).until(
         )
 
 username = login_box.find_element(By.TAG_NAME,'input')
-username.send_keys("write ur username")
+username.send_keys("write ur username ")
 next_button=login_box.find_element(By.XPATH,"//div[@class='css-18t94o4 css-1dbjc4n r-sdzlij r-1phboty r-rs99b7 r-ywje51 r-usiww2 r-2yi16 r-1qi8awa r-1ny4l3l r-ymttw5 r-o7ynqc r-6416eg r-lrvibr r-13qz1uu']")
 next_button.click()
 print('successfully logged in')
@@ -33,7 +33,7 @@ time.sleep(20)
 password = driver.find_element(By.XPATH, "//input[@name='password']")   
 print('successfully logged in')
 
-password.send_keys("write ur password ")
+password.send_keys("write ur passw")
 print('successfully logged in')
 login_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//div[@data-testid="LoginForm_Login_Button"]'))
@@ -66,16 +66,17 @@ print('search successfully')
 
 # search_button=driver.find_element(By.XPATH,'//div[@class="css-1dbjc4n r-7q8q6z r-6koalj r-1777fci"]')
 # search_button.click()
-print('search successfully')
-time.sleep(63)
-section =main_page.find_element(By.XPATH,'//section[@aria-labelledby="accessible-list-7"]')
-tweets=section.find_elements(By.XPATH,'//div[@data-testid="cellInnerDiv"]')
+time.sleep(13)
+section =main_page.find_element(By.XPATH,'//section[@role="region"]')
+tweets=section.find_elements(By.XPATH,'//article[@data-testid="tweet"]')
+print('found succ')
 user_data = []
 text_data = []
 for tweet in tweets:
-    name =tweet.find_elements(By.XPATH,'.//a[@class="css-4rbku5 css-18t94o4 css-1dbjc4n r-1loqt21 r-1wbh5a2 r-dnmrzs r-1ny4l3l"]//span[contains(text(), "@")]').text
+    text=tweet.find_element(By.XPATH,'.//div[@data-testid="tweetText"]').text
+    print('good')
+    name =tweet.find_element(By.XPATH,'.//span[contains(text(), "@")]').text
 
-    text=tweet.find_elements(By.XPATH,'.//div[@data-testid="tweetText"]').text
     user_data.append(name)  # appending the first element of tweet_list (user)
     text_data.append(text)
     print('secc')
